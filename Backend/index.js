cbeeb6e87e61b5e9fe952aebe2ec224754386e24
jeff rey <geffrey1182002@gmail.com>
2024-02-19 22:15:40 +0530

    Added image uploading and basic api query for backend

--- Backend/index.js
+++ Backend/index.js
@@ -16,12 +16,51 @@ app.use(cors())
 // user -jeffreydb  , pass - 123321wwe
 
 
-mongoose.connect("mongodb+srv://jeffreydb:123321wwe@cluster0.ifm647s.mongodb.net/")
+// user-jeffbro4lyf pass-jeffrey
+
+/* connecting database */
+// mongoose.set('strictQuery', false);
+// mongoose.connect("mongodb+srv://jeffreydb:123321we@cluster0.ifm647s.mongodb.net/", {dbName : "Ecommerce"})
+// .then( () => console.log('connected to database'))
+// .catch(err=> console.error("not connected to database ",err));
+
+
+mongoose.connect("?retryWrites=true&w=majority/EcommerceApp")
+    .then((res) => {
+    console.log("Connected")
+    })
+    .catch((err) => {
+    console.log(errs)
+})
 
 // API Creation
 
 app.get('/', (req , res) => {
     res.send("Express app is running")
+})
+
+// Image storage engine
+
+const storage = multer.diskStorage({
+    destination: './Upload/Images',
+    filename: (req, file, cb) => {
+        return cb(null , `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const Upload = multer({
+    storage:storage
+})
+
+// Creating Upload
+
+app.use('/images', express.static('upload/images'))
+
+app.post("/upload", Upload.single('product'), (req, res) => {
+    res.json({
+        Succes: 1,
+        Image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
 })
 
 app.listen(port, (err) => {
